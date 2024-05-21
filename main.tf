ef257ff059cd165c31089641fd21a1eb3c6d2f8b
Mallika <kmallika.malli@gmail.com>
2024-05-21 13:51:08 +0530

    added data "external" "thumb"

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_eks_node_group" "node-group" {
 }
 
 
-/*data "external" "thumb" {
+data "external" "thumb" {
   program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.eks.identity.0.oidc.0.issuer]
 }
 
@@ -45,4 +45,4 @@ resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.external.thumb.result.thumbprint]
   url             = aws_eks_cluster.eks.identity.0.oidc.0.issuer
-}*/
+}
